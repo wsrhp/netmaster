e282d2d277466cae3661c3623e5b557840491b4a
wsrhp <276531912@qq.com>
2016-07-21 10:42:20 +0800

    details changes
    details changes

--- computers/models.py
+++ computers/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Computer(models.Model):
     positionID = models.IntegerField(default=0)
     name = models.CharField(max_length=128, null=True, blank=True)
-    occupied = models.BooleanField(default=False)
     user = models.CharField(max_length=128, null=True, blank=True)
+    occupied = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.name
